6bd935326bfaf15f44e72115ab1203b1310ba27b
luan.lipperthoffmann <luan.lippert91@rede.ulbra.br>
2024-03-18 21:41:12 -0300

    att certificacao c#

--- C#_CeritificaçãoMicrosoft/obj/Debug/net8.0/C#_CeritificaçãoMicrosoft.AssemblyInfo.cs
+++ C#_CeritificaçãoMicrosoft/obj/Debug/net8.0/C#_CeritificaçãoMicrosoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_CeritificaçãoMicrosoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd6bce2c2bc4bba7a74bc0f66baa078292d1fed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13002c207d54715e08373f585835df3949fc601")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_CeritificaçãoMicrosoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_CeritificaçãoMicrosoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
